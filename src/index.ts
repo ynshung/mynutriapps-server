f0d042407236a3ccdb9b76c04a631c097a5b859d
Young Shung <me@ynshung.com>
2025-04-10 18:44:51 +0800

    use 0.0.0.0 to expose to public

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
-import os from "os";
 
 dotenv.config();
 
@@ -21,16 +20,4 @@ const startServer = (host: string) => {
 };
 
 startServer('127.0.0.1');
-const getCurrentIP = (): string => {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]!) {
-      if (iface.family === "IPv4" && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  throw new Error("Unable to determine current IP address");
-};
-
-startServer(getCurrentIP());
+startServer('0.0.0.0');
