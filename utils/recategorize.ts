f68999b22fcc88b1e13ad703db4f313988e93c86
Young Shung <me@ynshung.com>
2025-04-21 00:37:07 +0800

    add util function to recategorize product with ai

---
+++ utils/recategorize.ts
@@ -0,0 +1,74 @@
+import { processFrontLabel } from "@/src/ai/productFrontLabel";
+import { db } from "@/src/db";
+import {
+  foodCategoryTable,
+  foodProductsTable,
+  imageFoodProductsTable,
+  imagesTable,
+} from "@/src/db/schema";
+import { and, eq } from "drizzle-orm";
+
+const recategorize = async (categoryID: number = 0) => {
+  const categoryItems = await db
+    .select({
+      id: foodProductsTable.id,
+      imageKey: imagesTable.imageKey,
+    })
+    .from(foodProductsTable)
+    .innerJoin(
+      imageFoodProductsTable,
+      eq(foodProductsTable.id, imageFoodProductsTable.foodProductId)
+    )
+    .innerJoin(imagesTable, eq(imageFoodProductsTable.imageId, imagesTable.id))
+    .where(
+      and(
+        eq(foodProductsTable.foodCategoryId, categoryID),
+        eq(imageFoodProductsTable.type, "front")
+      )
+    );
+
+  for (const item of categoryItems) {
+    const imageKey = item.imageKey;
+    const foodProductID = item.id;
+    let newCategoryID = 0;
+
+    const image = await fetch(
+      `https://mna-sg.s3.ap-southeast-1.amazonaws.com/${imageKey}`
+    );
+    const imageArrayBuffer = await image.arrayBuffer();
+    const imageBuffer = Buffer.from(imageArrayBuffer);
+
+    const frontLabelData = await processFrontLabel(imageBuffer);
+
+    if (!frontLabelData)
+      throw new Error("Front label data couldn't be processed");
+    if (!frontLabelData.category) frontLabelData.category = "Uncategorized";
+    const categoryQueryResult = await db
+      .select()
+      .from(foodCategoryTable)
+      .where(eq(foodCategoryTable.name, frontLabelData.category));
+    if (categoryQueryResult.length === 0) {
+      throw new Error("Unexpected error: Category not found");
+    } else {
+      newCategoryID = categoryQueryResult[0].id;
+    }
+    const newProductValues = {
+      name: frontLabelData.name,
+      brand: frontLabelData.brand,
+      foodCategoryId: newCategoryID ?? 0, // Uncategorized if undefined
+    };
+
+    if (categoryID !== newCategoryID) {
+      await db
+        .update(foodProductsTable)
+        .set(newProductValues)
+        .where(eq(foodProductsTable.id, foodProductID));
+  
+      console.log(
+        `\u001b[1;32mRecategorized ${frontLabelData.name} (${frontLabelData.brand}) to ${frontLabelData.category}\u001b[0m`
+      );
+    }
+  }
+};
+
+recategorize(0);
