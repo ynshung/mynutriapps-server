43d347416e3ef51485bb29051368b1f2d0c914b7
Young Shung <me@ynshung.com>
2025-06-05 22:11:25 +0800

    add logging for db queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,18 @@
 import "dotenv/config";
+import { DefaultLogger, LogWriter } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { loggerDB } from "../utils/logger";
 
 const databaseUrl =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_DATABASE_URL
     : process.env.PROD_DATABASE_URL;
-export const db = drizzle(databaseUrl!, { logger: true });
+
+class MyLogWriter implements LogWriter {
+  write(message: string) {
+    loggerDB.info(message);
+  }
+}
+const logger = new DefaultLogger({ writer: new MyLogWriter() });
+
+export const db = drizzle(databaseUrl!, { logger });
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,29 @@
 import winston from "winston";
 
+const { combine, timestamp, printf } = winston.format;
+
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
 export const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: combine(timestamp(), customFormat),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
+    new winston.transports.Console({ format: combine(timestamp(), customFormat) }),
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "warn.log", level: "warn" }),
     new winston.transports.File({ filename: "full.log" }),
+  ],
+});
+
+export const loggerDB = winston.createLogger({
+  level: "info",
+  format: combine(timestamp(), customFormat),
+  defaultMeta: { service: "db-service" },
+  transports: [
+    new winston.transports.Console({ format: combine(timestamp(), customFormat) }),
+    // new winston.transports.File({ filename: "db.log" }),
   ],
 });
